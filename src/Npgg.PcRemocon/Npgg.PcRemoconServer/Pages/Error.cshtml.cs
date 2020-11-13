3124b013df74c33911f063247e79211b31c0ac4c
Newp <mynewp@gmail.com>
2020-11-13 20:23:46 +0900

    기본적인 키입력 추가

--- src/Npgg.PcRemocon/Npgg.PcRemoconServer/Pages/Error.cshtml.cs
+++ src/Npgg.PcRemocon/Npgg.PcRemoconServer/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Npgg.PcRemocon.Pages
+namespace Npgg.PcRemoconServer.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
